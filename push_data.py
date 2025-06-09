1991d05de39c84ee3f885406696d300bc7114023
Arockia Anand Raj D <aroanand21f@gmail.com>
2025-06-09 15:54:05 +0530

    ETL Pipeline completed

--- push_data.py
+++ push_data.py
@@ -1,76 +1,67 @@
 import os
 import sys
 import json
+
 from dotenv import load_dotenv
-import pymongo.client_options
+
 load_dotenv()
 
-MONGO_DB_URL =  os.getenv("MONGO_DB_URL")
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+# print(MONGO_DB_URL)
 
 
-import certifi
-ca = certifi.where()  ### Secure the Http connection. 
-
-
-## Read the Dataset
+import certifi # That provides set root certificates and secure http connection
+ca = certifi.where()
 
 import pandas as pd
 import numpy as np
 import pymongo
-from networksecurity.exception.exception import NetworkSecurityException
+
 from networksecurity.logging.logger import logging
+from networksecurity.exception.exception import NetworkSecurityException
 
 
 class NetworkDataExtract():
-    """This is ETL Pipeline which is responsible for extract data from database and transform it then load into other database"""
-
     def __init__(self):
 
         try:
             pass
-
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-
-    def csv_to_json(self, file_path):
-        """This funtion converts the data from csv into json format"""
-
+    def cv_to_json_converter(self, file_path):
         try:
             data = pd.read_csv(file_path)
-            data.reset_index(drop=True, inplace=True)
+            # data.reset_index(drop=True, inplace=True)
             records = list(json.loads(data.T.to_json()).values())
             return records
-        
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
- 
-    def insert_data_into_mongodb(self, records, database, collection):
-        """This function load json format data into the MongoDB Atlas."""
-
+        
+    def insert_data_mongodb(self, database, collection, records):   #[Tables -> Collection, Values-> Document(key-value pair)]
         try:
             self.database = database
-            self.records = records
             self.collection = collection
+            self.records = records
 
-            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
-            self.database = self.mongo_client[self.database]
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)   # Connect to MongoDB
+            self.database = self.mongo_client[self.database]        # Connect to Database
+            self.collection = self.database[self.collection]        # Connect to (Table)Collection[list of jsons[{},{},{}]]
+            self.collection.insert_many(self.records)               # This line insert the values to json format [{"A"":1"}, {"B"":2"}]
 
-            self.collection = self.database[self.collection]
-            self.collection.insert_many(self.records)
-
-            return(len(self.records))
-
+            return (len(self.records))
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
 
 if __name__ == "__main__":
     FILE_PATH = "Network_Data\\PhisingData.csv"
-    DATABASE = "PROJECTDB"
-    Collection = "NetworkData"
-    networkobj = NetworkDataExtract()
-    records = networkobj.csv_to_json(FILE_PATH)
-    print(records)
-    no_of_records = networkobj.insert_data_into_mongodb(records, DATABASE, Collection)
+    DATABASE = "Anand_updated_database"
+    Collection = "NetworkDATA"  ##It's similar to table name
+    network_obj = NetworkDataExtract()
+    records = network_obj.cv_to_json_converter(file_path=FILE_PATH)
+    no_of_records = network_obj.insert_data_mongodb(DATABASE, Collection, records)
+
     print(no_of_records)
